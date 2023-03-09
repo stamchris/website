d9cc4d9cf733899681ef0932090b5244f3086aff
Christos Stamatopoulos <80109501+stamchris@users.noreply.github.com>
2023-03-09 12:23:19 +0200

    Hash Router added

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 import React from 'react'
-import {Routes,Route,Link} from "react-router-dom"
+import {Routes,Route,HashRouter} from "react-router-dom"
 import { Home,Terms,Policy,AboutUs} from './pages';
 
 const App = () => 
 		 (
+			<HashRouter>
 				<Routes>
 					<Route exact path="/" element={<Home/>} />
 					<Route path="/aboutUs" element={<AboutUs/>} />
 					<Route path="/terms" element={<Terms/>} />
 					<Route path="/policy" element={<Policy/>} />
-				</Routes> 	
+				</Routes> 
+			</HashRouter>	
 		)
 
 export default App;
