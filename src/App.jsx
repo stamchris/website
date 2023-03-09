ae0218d7b79d401626f3a2949e6b754eed2aa53e
Christos Stamatopoulos <80109501+stamchris@users.noreply.github.com>
2023-03-09 19:14:46 +0200

    new router path

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,15 @@
 import React from 'react'
-import {Routes,Route,HashRouter} from "react-router-dom"
+import {Routes,Route} from "react-router-dom"
 import { Home,Terms,Policy,AboutUs} from './pages';
 
 const App = () => 
 		 (
-			<HashRouter>
 				<Routes>
-					<Route exact path="/" element={<Home/>} />
-					<Route path="/aboutUs" element={<AboutUs/>} />
-					<Route path="/terms" element={<Terms/>} />
-					<Route path="/policy" element={<Policy/>} />
-				</Routes> 
-			</HashRouter>	
+					<Route exact path="website/" element={<Home/>} />
+					<Route path="website/aboutUs" element={<AboutUs/>} />
+					<Route path="website/terms" element={<Terms/>} />
+					<Route path="website/policy" element={<Policy/>} />
+				</Routes> 	
 		)
 
 export default App;
