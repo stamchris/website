83ad8b93b77ee387dc61a390bc241b79e24daf99
Christos Stamatopoulos <80109501+stamchris@users.noreply.github.com>
2023-03-14 14:26:09 +0200

    New Layout v2

--- src/components/Illustration.jsx
+++ src/components/Illustration.jsx
@@ -3,21 +3,21 @@ import { productItems} from '../constants'
 
 const Illustration = () =>(
 
-        <section id='product' className={`flex flex-col w-full md:h-[600px] justify-center items-center mt-10`}>
+        <section id='product' className={`flex flex-col w-full  justify-center items-center mt-10`}>
 
             {productItems.map((item,index) =>(
-                <div className='flex-1 flex flex-row even:flex-row-reverse gap-10 p-4 w-full h-[200px] md:h-[300px] bg-white m-2 justify-start items-center drop-shadow-sm rounded-lg z-30' key={index}>
+                <div className='flex-1 flex flex-row even:flex-row-reverse gap-10 p-4 w-full h-[200px] md:h-[300px] bg-white m-2 justify-between items-center drop-shadow-sm rounded-lg z-30' key={index}>
 
                     <img src={`${item.img}`} alt="illustration"
-                    className='h-[200px] w-[200px] z-30 '
+                    className='h-[200px] w-[200px] sm:h-[300px] sm:w-[300px] md:h-[400px] md:w-[400px] z-30 '
                     />
                         
-                    <div>    
-                        <h1 className='text-emerald-500 text-2xl text-center text-bold p-4 my-4 rounded-lg md:text-3xl md:min-h-[140px] z-30'>
+                    <div className=''>    
+                        <h1 className=' text-gray-600 text-3xl text-center text-bold p-4 my-4 rounded-lg md:text-4xl md:min-h-[140px] z-30'>
                                 {item.title}
                         </h1>
                         <p
-                            className='text-sm py-3 bg-white rounded-sm px-1 my-4 border-l-4 border-emerald-400 text-gray-700 md:text-md z-30'
+                            className='text-xs sm:text-sm md:text-md max-w-[700px] py-3 bg-white rounded-sm px-1 my-4 border-l-4 border-emerald-400 text-gray-700  z-30'
                         >
                             {item.desc}
                         </p>
